402869c813cf688416638218e1fd278408dca2b8
Leo <lyevart@gmail.com>
2020-08-18 16:41:19 +0200

    0.1 Version
    Basic random geneator, design, and kinda playing with p5.js on the background.
    
    Database still very limited and ideas are incongruent, but it works.

---
+++ precache-manifest.3620f1e3df988fac9158eb66da81934c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d4202978cce185d6a8d405addee714",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51e5343e082872cbb5fc",
+    "url": "./static/css/main.c1431252.chunk.css"
+  },
+  {
+    "revision": "c48c3e103e464983c983",
+    "url": "./static/js/2.8d90bead.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8d90bead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51e5343e082872cbb5fc",
+    "url": "./static/js/main.3e6d0e97.chunk.js"
+  },
+  {
+    "revision": "e3b1cd9b3d8efbb3bb4d",
+    "url": "./static/js/runtime-main.8350a47b.js"
+  }
+]);
